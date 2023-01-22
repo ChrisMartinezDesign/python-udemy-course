cce1fba59efbba79511b416aa55a35183f6750bb
Chris Martinez <103085160+chrisMartinezDeveloper@users.noreply.github.com>
2023-01-22 13:45:53 -0500

    Add files via upload

---
+++ day_1.py
@@ -0,0 +1,11 @@
+# Write your code below this line 👇
+# Comment Syntax
+name = input("What is your name?\n")
+print("Name: " + name)
+
+
+# Fix the code below 👇
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the " + " sign.")
+print('e.g. print("Hello " + "world")')
+print(("New lines can be created with a backslash and n."))
